ef15751faa1ab054d837ac6ddbeb751af4978138
Lucky Dinawanao <lucky_dinawanao@ligph.com>
2018-06-18 18:15:38 +0800

    Adding Search Property section (not done)

---
+++ wp/wp-content/themes/sampletheme/modules/search-properties.php
@@ -0,0 +1,74 @@
+<section class="search-property">
+  <div class="l-container">
+    <div class="search-property-wrapper">
+      <h2 class="search-property-title">物件を検索する</h2>
+      <div class="search-property-form">
+        <form class="form u-clear" method="post" action="/">
+          <div class="form-wrap form-left">
+            <h3 class="form-group-title">賃料検索</h3>
+            <div class="form-group form-select-wrap">
+              <select class="form-input form-select" name="search-rent-from">
+                <option>0000万円</option>
+                <option>10000万円</option>
+                <option>50000万円</option>
+                <option>100000万円</option>
+                <option>500000万円</option>
+                <option>1000000万円</option>
+              </select>
+            </div>
+            <div class="form-sep flex flex-align-center">
+              <span>~</span>
+            </div>
+            <div class="form-group form-select-wrap">
+              <select class="form-input form-select" name="search-rent-to">
+                <option>0000万円</option>
+                <option>10000万円</option>
+                <option>50000万円</option>
+                <option>100000万円</option>
+                <option>500000万円</option>
+                <option>1000000万円</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-wrap form-right">
+            <h3 class="form-group-title">総額検索</h3>
+            <div class="form-group form-select-wrap">
+              <select class="form-input form-select" name="total-price-from">
+                <option>0000万円</option>
+                <option>10000万円</option>
+                <option>50000万円</option>
+                <option>100000万円</option>
+                <option>500000万円</option>
+                <option>1000000万円</option>
+              </select>
+            </div>
+            <div class="form-sep flex flex-align-center">
+              <span>~</span>
+            </div>
+            <div class="form-group form-select-wrap">
+              <select class="form-input form-select" name="total-price-to">
+                <option>0000万円</option>
+                <option>10000万円</option>
+                <option>50000万円</option>
+                <option>100000万円</option>
+                <option>500000万円</option>
+                <option>1000000万円</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-wrap form-submit">
+            <button class="btn form-btn" type="submit">この条件で検索する</button>
+          </div>
+        </form>
+      </div>
+
+      <div class="search-property-buttons">
+        <ul class="row">
+          <li class="col-lg-4"><a class="btn" href="">お問い合わせ</a></li>
+          <li class="col-lg-4"><a class="btn" href="">個別で内覧依頼する</a></li>
+          <li class="col-lg-4"><a class="btn" href="">お気に入りに追加する</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
