8318013a5276d14d70c8dd74d0e3295be93237ea
Mark <mark_win_panerio@ligph.com>
2018-06-19 16:29:51 +0800

    page town spot init

---
+++ wp/wp-content/themes/sampletheme/svg/icon-blank.php
@@ -0,0 +1,8 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 40.02 40.02">
+  <title>icon_brank</title>
+  <polygon points="36 19.02 36 36.02 4 36.02 4 4.02 21 4.02 21 0.02 0 0.02 0 4.02 0 4.02 0 36.02 0 36.02 0 40.02 40 40.02 40 40.02 40 40.02 40 19.02 36 19.02"/>
+  <rect x="36" y="0.02" width="4" height="15"/>
+  <rect x="25.02" width="15" height="4"/>
+  <rect x="16.11" y="8.74" width="26.3" height="4" transform="translate(0.98 23.85) rotate(-45)"/>
+  <rect x="18.6" y="18.62" width="2.8" height="2.8"/>
+</svg>
