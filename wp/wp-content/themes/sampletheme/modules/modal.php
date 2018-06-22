d21d6a9f66b6f801eef39c26cf12056e092e337b
Clint Eric Mosqueda <clint_mosqueda@ligph.com>
2018-06-22 09:18:57 +0800

    update modal module

--- wp/wp-content/themes/sampletheme/modules/modal.php
+++ wp/wp-content/themes/sampletheme/modules/modal.php
@@ -3,10 +3,8 @@
     <div class="modal-bg"></div>
     <div class="modal-content-wrapper js-content-wrapper">
       <div class="modal-content js-content l-container">
-        <div class="close-button js-close">close</div>
-        <p>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test
-          message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message<br>test message</p>
-        <div class="close-button js-close">close</div>
+        <a class="close-button js-close" href="javascript:;"></a>
+        <img class="modal-image" src="<?php echo resolve_asset_uri('images/gmap.jpg'); ?>" alt="">
       </div>
     </div>
   </div>
