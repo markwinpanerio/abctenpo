97a91f1e8583ccf3ecc32befaa687559d5249c24
Belle Jema <belle_jema@ligph.com>
2018-06-22 14:53:58 +0800

    updated virtual module

--- wp/wp-content/themes/sampletheme/modules/virtual.php
+++ wp/wp-content/themes/sampletheme/modules/virtual.php
@@ -1,7 +1,7 @@
 <div class="virtual<?php echo ($virtualModifier != '' ? ' '.$virtualModifier : ''); ?>">
   <div class="virtual-left virtual-top-item">
     <div class="virtual-left-inner<?php echo ($innerModifier != '' ? ' '.$innerModifier : ''); ?>">
-      <iframe src="https://www.google.com/maps/embed?pb=!4v1529635463336!6m8!1m7!1sCAoSLEFGMVFpcE01ZDh2b1pHTm9YbWNucVR4NkxORnJ2aGgxNFI4dlBRU2VWTDhf!2m2!1d10.31923621547137!2d123.9030895481028!3f314.19658539695394!4f0.28764085743584644!5f0.7820865974627469" class="<?php echo $iframeModifier; ?>" allowfullscreen></iframe>
+      <iframe src="<?php echo $iframeUrl; ?>" class="<?php echo $iframeModifier; ?>" allowfullscreen></iframe>
     </div>
   </div>
   <div class="virtual-right virtual-top-item">
