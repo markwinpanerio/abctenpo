5ab2c6081d58b3d4719713cc55e7300a6d3ca11e
Belle Jema <belle_jema@ligph.com>
2018-06-25 15:56:10 +0800

    updated virtual modules

--- wp/wp-content/themes/sampletheme/index.php
+++ wp/wp-content/themes/sampletheme/index.php
@@ -141,33 +141,6 @@ get_header(); ?>
         <a href="#" class="listing-button">もっと見る</a>
       </section>
 
-
-      <!-- start of virtual section -->
-      <?php importTemplate('modules/virtual', array(
-        'virtualModifier' => '',
-        'virtualTopItemFirstModifier' => '',
-        'virtualTopItemSecondModifier' => '',
-        'virtualLeftCopyModifier' => '',
-        'innerModifier' => '',
-        'iframeModifier' => '',
-        'iframeUrl' => 'https://www.google.com/maps/embed?pb=!4v1529635463336!6m8!1m7!1sCAoSLEFGMVFpcE01ZDh2b1pHTm9YbWNucVR4NkxORnJ2aGgxNFI4dlBRU2VWTDhf!2m2!1d10.31923621547137!2d123.9030895481028!3f314.19658539695394!4f0.28764085743584644!5f0.7820865974627469',
-        'heading' => '3Dバーチャル内覧とは？',
-        'rightHeadingModifier' => '',
-        'leftCopy' => 'ドラッグやスワイプで360°ご覧いただけます',
-        'rightCopy' => '店舗の中を360°見渡せる、3Dバーチャル<br class="u-pc-hidden">内覧です。<br>圧倒的な臨場感を味わえるように床やキッチンの質感<br class="u-sp-hidden">まで忠実に再現。<br class="u-sp-hidden">まるでその物件に居るような体験ができます。',
-        'rightCopyModifier' => '',
-        'virtualButtonText' => '',
-        'virtualButtonLink' => '',
-        'virtualButtonTarget' => '',
-        'svgIcons' => array(
-          'svgIcon' => array(
-            'svg-icon' => 'svg/mouse.svg',
-            'svg-modifier' => ''
-          )
-        )
-      )); ?>
-      <!-- end of virtual section -->
-
       <!-- start of listing section -->
       <section class="listing">
         <?php importTemplate('modules/listing-list', array(
@@ -204,6 +177,21 @@ get_header(); ?>
       </section>
       <!-- end of listing section -->
 
+      <!-- start of virtual section -->
+      <?php importTemplate('modules/virtual', array(
+        'modifier'        => 'first',
+        'iframeUrl'       => 'https://www.google.com/maps/embed?pb=!4v1529635463336!6m8!1m7!1sCAoSLEFGMVFpcE01ZDh2b1pHTm9YbWNucVR4NkxORnJ2aGgxNFI4dlBRU2VWTDhf!2m2!1d10.31923621547137!2d123.9030895481028!3f314.19658539695394!4f0.28764085743584644!5f0.7820865974627469',
+        'heading'         => '3Dバーチャル内覧とは？',
+        'leftCopy'        => 'ドラッグやスワイプで360°ご覧いただけます',
+        'rightCopy'       => '店舗の中を360℃見渡せる、3Dバーチャル内覧です。<br>圧倒的な臨場感を味わえる、床やキッチンの質感<br class="u-sp-hidden">まで忠実に再現。<br>まるでその物件に居るような体験ができます。 ',
+        'svgIcon'         => 'svg/mouse.svg',
+        'buttonText'      => '',
+        'buttonLink'      => 'javascript',
+        'buttonTarget'    => '_blank',
+        'svgButtonIcon'   => 'svg/pdf.svg'
+      )); ?>
+      <!-- end of virtual section -->
+
     </div>
     <div class="l-container-sidebar">
       <?php importPart('sidebar'); ?>
@@ -215,27 +203,16 @@ get_header(); ?>
 <!-- start of virtual second section -->
 <div class="l-container">
   <?php importTemplate('modules/virtual', array(
-    'virtualModifier' => 'virtual-second',
-    'virtualTopItemFirstModifier' => 'virtual-second-top-item-first',
-    'virtualTopItemSecondModifier' => '',
-    'virtualLeftCopyModifier' => 'virtual-second-left-copy',
-    'innerModifier' => 'virtual-second-left-inner',
-    'iframeModifier' => 'virtual-second-iframe',
-    'iframeUrl' => 'https://www.google.com/maps/embed?pb=!4v1529635463336!6m8!1m7!1sCAoSLEFGMVFpcE01ZDh2b1pHTm9YbWNucVR4NkxORnJ2aGgxNFI4dlBRU2VWTDhf!2m2!1d10.31923621547137!2d123.9030895481028!3f314.19658539695394!4f0.28764085743584644!5f0.7820865974627469',
-    'heading' => '店内3Dバーチャル内覧開催中',
-    'rightHeadingModifier' => 'virtual-second-right-heading',
-    'leftCopy' => 'ドラッグやスワイプで360°ご覧いただけます',
-    'rightCopy' => '店舗の中を360℃見渡せる、3Dバーチャル内覧です。<br>圧倒的な臨場感を味わえる、床やキッチンの質感まで忠実に再現。<br>まるでその物件に居るような体験ができます。 ',
-    'rightCopyModifier' => 'virtual-second-right-copy',
-    'virtualButtonText' => '平面図を見る',
-    'virtualButtonLink' => 'javascript',
-    'virtualButtonTarget' => '_blank',
-    'svgIcons' => array(
-      'svgIcon' => array(
-        'svg-icon' => 'svg/mouse.svg',
-        'svg-modifier' => 'virtual-second-svg'
-      )
-    )
+    'modifier'        => 'second',
+    'iframeUrl'       => 'https://www.google.com/maps/embed?pb=!4v1529635463336!6m8!1m7!1sCAoSLEFGMVFpcE01ZDh2b1pHTm9YbWNucVR4NkxORnJ2aGgxNFI4dlBRU2VWTDhf!2m2!1d10.31923621547137!2d123.9030895481028!3f314.19658539695394!4f0.28764085743584644!5f0.7820865974627469',
+    'heading'         => '店内3Dバーチャル内覧開催中',
+    'leftCopy'        => 'ドラッグやスワイプで360°ご覧いただけます',
+    'rightCopy'       => '店舗の中を360℃見渡せる、3Dバーチャル内覧です。<br>圧倒的な臨場感を味わえる、床やキッチンの質感まで忠実に再現。<br class="u-sp-hidden">まるでその物件に居るような体験ができます。 ',
+    'svgIcon'         => 'svg/mouse.svg',
+    'buttonText'      => '平面図を見る',
+    'buttonLink'      => 'javascript',
+    'buttonTarget'    => '_blank',
+    'svgButtonIcon'   => 'svg/pdf.svg'
   )); ?>
 </div>
 <!-- end of virtual second section -->
--- wp/wp-content/themes/sampletheme/modules/virtual.php
+++ wp/wp-content/themes/sampletheme/modules/virtual.php
@@ -1,19 +1,39 @@
-<div class="virtual<?php echo ($virtualModifier != '' ? ' '.$virtualModifier : ''); ?>">
-  <div class="virtual-left virtual-top-item<?php echo ($virtualTopItemFirstModifier != '' ? ' '.$virtualTopItemFirstModifier : ''); ?>">
-    <div class="virtual-left-inner<?php echo ($innerModifier != '' ? ' '.$innerModifier : ''); ?>">
-      <iframe src="<?php echo $iframeUrl; ?>" class="virtual-iframe<?php echo ($iframeModifier != '' ? ' '.$iframeModifier : ''); ?>" allowfullscreen></iframe>
-      <p class="virtual-left-copy<?php echo ($virtualLeftCopyModifier != '' ? ' '.$virtualLeftCopyModifier : ''); ?>">
-        <?php foreach ($svgIcons as $svgIcon): ?>
-          <img src="<?php echo resolve_asset_uri('images/"'.$svgIcon['svg-icon']); ?>" class="virtual-svg<?php echo ($svgIcon['svg-modifier'] != '' ? ' '.$svgIcon['svg-modifier'] : ''); ?>" alt="">
-         <?php endforeach; ?>
-        <?php echo $leftCopy; ?></p>
+<?php
+
+  $module = 'virtual';
+  $array  = array();
+
+  if($modifier != '') {
+    $array[0] = ' ' .$module .'-'. $modifier;
+    $array[1] = ' ' .$module .'-left-'. $modifier;
+    $array[2] = ' ' .$module .'-left-inner-'. $modifier;
+    $array[3] = ' ' .$module .'-iframe-'. $modifier;
+    $array[4] = ' ' .$module .'-left-copy-'. $modifier;
+    $array[5] = ' ' .$module .'-svg-'. $modifier;
+    $array[6] = ' ' .$module .'-right-'. $modifier;
+    $array[7] = ' ' .$module .'-right-heading-'. $modifier;
+    $array[8] = ' ' .$module .'-right-copy-'. $modifier;
+    $array[9] = ' ' .$module .'-button-icon-'. $modifier;
+  }
+
+  echo
+  '<div class="virtual'.$array[0].'">
+    <div class="virtual-left virtual-top-item'.$array[1].'">
+      <div class="virtual-left-inner'.$array[2].'">
+        <iframe src="'.$iframeUrl.'" class="virtual-iframe'.$array[3].'" allowfullscreen></iframe>
+        <p class="virtual-left-copy'.$array[4].'">
+            <img src="'.resolve_asset_uri('images/"'.$svgIcon).'" class="virtual-svg'.$array[5].'" alt="">
+            '.$leftCopy.'</p>
+      </div>
     </div>
-  </div>
-  <div class="virtual-right virtual-top-item<?php echo ($virtualTopItemSecondModifier != '' ? ' '.$virtualTopItemSecondModifier : ''); ?>">
-    <h3 class="virtual-right-heading<?php echo ($rightHeadingModifier != '' ? ' '.$rightHeadingModifier : ''); ?>"><?php echo $heading; ?></h3>
-    <p class="virtual-right-copy<?php echo ($rightCopyModifier != '' ? ' '.$rightCopyModifier : ''); ?>"><?php echo $rightCopy; ?></p>
-    <?php if($virtualButtonTarget != '') : ?>
-      <a href="<?php echo $virtualButtonLink; ?>" target="<?php echo $virtualButtonTarget; ?>" class="virtual-button"><?php echo $virtualButtonText; ?></a>
-    <?php endif; ?>
-  </div>
-</div>
+    <div class="virtual-right virtual-top-item'.$array[6].'">
+      <h3 class="virtual-right-heading'.$array[7].'">'.$heading.'</h3>
+      <p class="virtual-right-copy'.$array[8].'">'.$rightCopy.'</p>'; ?>
+
+    <?php if($buttonText != '') :
+      echo '<a href="'.$buttonLink.'" target="'.$buttonTarget.'" class="virtual-button"><span class="virtual-button-icon'.$array[9].'"><img src="'.resolve_asset_uri('images/"'.$svgButtonIcon).'" alt=""></span>'.$buttonText.'</a>';
+       endif;
+      echo
+      '</div>
+    </div>';
+?>
